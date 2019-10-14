adad9a01c3531f36793b13dabb6fb9c87e30829d
Sander Homan <sander.homan@wordtango.net>
2019-10-14 21:27:40 +0200

    Added home description Basic skills display

--- cv_react/src/Skills.tsx
+++ cv_react/src/Skills.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Data } from './datatypes';
+import { PromiseWrapperConsumer } from './Util';
+import Spinner from './components/Spinner';
 
-const Skills: React.FC = () => (
-  <div>skills</div>
-)
+
+const Skills: React.FC<PromiseWrapperConsumer<Data>> = ({data, error, completed}) => {
+  if (completed && data) {
+    const e = Object.keys(data.Skills).map(key => <div>{key}: {data.Skills[key].join(", ")}</div>);
+    return <React.Fragment>{e}</React.Fragment>;
+  }
+  return <Spinner/>;
+}
 
 export default Skills;
